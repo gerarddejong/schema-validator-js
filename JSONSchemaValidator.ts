26783da156f4321f4e5928958b747d8e869ef47e
Gerard de Jong <gerard@dejong.za.net>
2016-10-24 12:00:29 +0200

    Got ts classes compiling

---
+++ JSONSchemaValidator.ts
@@ -0,0 +1,5 @@
+class JSONSchemaValidator implements SchemaValidator {
+  validate(unvalidated: Object, schema: JSON) : SchemaValidationResult {
+    return SchemaValidationResult.invalidResultWithMessage("No implemented yet!");
+  }
+}
--- SchemaValidationResult.ts
+++ SchemaValidationResult.ts
@@ -1,4 +1,18 @@
 class SchemaValidationResult {
   isValid: Boolean;
   message: String;
+
+  static validResultWithMessage(message: String) : SchemaValidationResult {
+    let validationResult: SchemaValidationResult = new SchemaValidationResult();
+    validationResult.isValid = true;
+    validationResult.message = message;
+    return validationResult;
+  }
+
+  static invalidResultWithMessage(message: String) : SchemaValidationResult {
+    let validationResult: SchemaValidationResult = new SchemaValidationResult();
+    validationResult.isValid = false;
+    validationResult.message = message;
+    return validationResult;
+  }
 }
